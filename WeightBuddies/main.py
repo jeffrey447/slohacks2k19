504dfbc48b8577a0814707dadedb189fec535fa8
Stanley Lee <stanleylee91776@gmail.com>
2019-02-02 11:38:16 -0800

    sdsd

--- WeightBuddies/main.py
+++ WeightBuddies/main.py
@@ -4,7 +4,7 @@ import datetime
 multiplier = 1
 initialCoin = 500
 
-# start of fake variables
+# st7art of fake variables
 fakePersonone = True  # Person one met goal
 fakePersontwo = True  # Person two met goal
 
@@ -19,24 +19,42 @@ fakeEnddate = datetime.datetime(2019, 1, 9)
 # end of fake variables
 
 
-def checkpoints(startWeight):
+def checkpoints(startWeight, endWeight, startdate):
     # Returns a tuple of two numbers: first one is when they reached 1/3rd of the goal, second is when they reached 2/3rd of goal
-    thirdway = startWeight / 3
-    checkPoints = (thirdway, thirdway * 2)
-    return checkPoints
+    weightTolose = startWeight - endWeight
+    thirdway = weightTolose / 5
+    checkpoints = [np.array([])]
+    checkDateone = startdate + datetime.timedelta(days=10)
+    checkDatetwo = startdate + datetime.timedelta(days=20)
+    checkpoints = np.append(checkpoints, [((thirdway * 2) + endWeight, checkDateone), (thirdway + endWeight, checkDatetwo)])
+    return checkpoints
 
 
-def ifBothreachGoal(personOne, personTwo, oneEndweight, twoEndweight):
+def ifBothreachGoal(personOneweight, personTwoweight, oneEndweight, twoEndweight):
     # Returns end coin count if both reach goal
-    if(personone <= oneEndweight and persontwo <= twoEndweight):
+    if(personOneweight <= oneEndweight and personTwoweight <= twoEndweight):
+        extraLostone = oneEndweight - personOneweight
+        extraLosttwo = twoEndweight - personTwoweight
+        return(multiplier * initialCoin + extraLosttwo + extraLostone)
+    elif(personOneweight <= oneEndweight or personTwoweight <= twoEndweight):
+        print("Only one person reached goal!")
 
 
-def checkpointOnereached(weight, date):
-    if(date ==)
+# def checkpointOnereached(weight, date, checkpoint):
+#     if(weight <= checkpoint[0][0] and date < checkpoint[0][1]):
+#         return multiplier + .5
 
 
-def checkPointTworeached(weight, date):
+# def checkPointTworeached(weight, date, checkpoint):
+#     if(weight <= checkpoint[1][0] and date < checkpoint[1][1]):
+#         return multiplier + .75
 
 
-print(ifBothreachGoal(fakePersonone, fakePersontwo, initialCoin))
-print(checkpoints(initialCoin))
+print(ifBothreachGoal(150, 200, 200, 300))
+
+firstCheck = checkpoints(fakeStartweight, fakeEndweight, fakeStartdate)
+print(firstCheck)
+secondCheck = checkpoints(400, 20, fakeStartdate)
+print(secondCheck)
+print(type(secondCheck[1]))
+# print(checkpointOnereached(200, datetime.datetime(2019, 1, 5), firstCheck))
